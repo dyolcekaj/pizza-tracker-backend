ab95bd489f39e37e4133ad051f8e7e2d6735f370
Jake Cloyd <dyolcekaj@gmail.com>
2017-03-11 12:06:42 -0600

    Add getTweets handler to index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const getContacts = require('./src/getContacts.js');
 const createContact = require('./src/createContact.js');
 const checkRecentOrders = require('./src/checkRecentOrders.js');
+const getTweets = require('./src/getTweets.js');
 
 exports.getContacts = getContacts;
 exports.createContact = createContact;
 exports.checkRecentOrders = checkRecentOrders;
+exports.getTweets = getTweets;
