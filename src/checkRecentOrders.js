ac2f99662a737ed01ca010e87970814f8bca61e8
Jake Cloyd <dyolcekaj@gmail.com>
2017-03-13 14:02:49 -0500

    Fixed xml parsing in dominos api. checkRecentOrders pretty much works

--- src/checkRecentOrders.js
+++ src/checkRecentOrders.js
@@ -48,6 +48,7 @@ function trackTheirPizza(contact) {
         contact.phoneNumber,
         (pizzaInfo) => {
             // This is a guess until I actually order a pizza
+            console.log(pizzaInfo);
             if (pizzaInfo.orders && pizzaInfo.orders.length > 0) {
             	console.log('[checkRecentOrders]: found order ', pizzaInfo);
             	sendTweet(contact, pizzaInfo);
